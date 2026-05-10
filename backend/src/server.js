17b6f9bae4aceb4f89c3278b63e84a5a1cc0e9d9
Shreya Tyagi <shreyaatyagii@gmail.com>
2026-05-11 03:32:37 +0530

    fix railway 502

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,4 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-dns.setDefaultResultOrder("ipv4first");
-const express = require("express");
+ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
